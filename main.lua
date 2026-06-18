53980b94202246f8da8f683fa3fd036a7034204c
eze <91032516+who-is-eze@users.noreply.github.com>
2026-06-18 19:40:04 -0400

    fixed drag for second gui (oops)

--- main.lua
+++ main.lua
@@ -277,18 +277,18 @@ UserInputService.InputChanged:Connect(function(input)
 
 local function update(input)
     local delta = input.Position - dragStart
-    mainFrame.Position = UDim2.new(
+    secondFrame.Position = UDim2.new(
         startPos.X.Scale,
         startPos.X.Offset + delta.X,
         startPos.Y.Scale,
         startPos.Y.Offset + delta.Y
     )
 end
-mainFrame.InputBegan:Connect(function(input)
+secondFrame.InputBegan:Connect(function(input)
         if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
             dragging = true
             dragStart = input.Position
-            startPos = mainFrame.Position
+            startPos = secondFrame.Position
             input.Changed:Connect(function()
                     if input.UserInputState == Enum.UserInputState.End then
                         dragging = false
@@ -296,7 +296,7 @@ mainFrame.InputBegan:Connect(function(input)
                 end)
             end
         end)
-mainFrame.InputChanged:Connect(function(input)
+secondFrame.InputChanged:Connect(function(input)
         if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
             dragInput = input
         end
